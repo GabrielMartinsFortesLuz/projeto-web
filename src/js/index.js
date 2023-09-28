acfec84e55d2ae6528677943ef7a661fec2efd65
Gabriel Luz <gabrielmfluz@gmail.com>
2023-09-28 11:39:37 -0300

    Renomear createLobby.html para lobby.html

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const showPopupButton = document.getElementById("showPopup");
+const closePopupButton = document.getElementById("closePopup");
+const confirmButton = document.getElementById("confirmButton");
+const overlay = document.getElementById("overlay");
+const popup = document.getElementById("popup");
+
+showPopupButton.addEventListener("click", function() {
+    overlay.style.display = "block";
+    popup.style.display = "block";
+});
+
+closePopupButton.addEventListener("click", function() {
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
+
+confirmButton.addEventListener("click", function() {
+    const lobbyNameInput = document.getElementById("lobbyName");
+    const lobbyName = lobbyNameInput.value.trim();
+
+    if (lobbyName !== "") {
+        window.location.href = `lobby.html?room=${lobbyName}`;
+    } else {
+        alert("Por favor, insira um nome de sala válido.");
+    }
+});
