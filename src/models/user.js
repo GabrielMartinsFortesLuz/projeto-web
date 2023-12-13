b4410d8648b33f98fda6d8af370a02fe3d03c47c
Gabriel Luz <gabrielmfluz@gmail.com>
2023-12-13 02:52:17 -0300

    Implementação inicial models

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const User = db.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+}, {
+  tableName: 'user',
+  timestamps: false
+});
+
+//Criar a tabela
+// User.sync();
+
+module.exports = User;
